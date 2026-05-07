1c78cb9c28ab184795d04a6988985fed63a30735
kenta-crv <okuyamakenta@gmail.com>
2026-05-07 20:44:30 +0900

    fix

--- db/migrate/20251208172754_add_service_type_to_columns.rb
+++
@@ -1,5 +0,0 @@
-class AddServiceTypeToColumns < ActiveRecord::Migration[6.1]
-  def change
-    add_column :columns, :genre, :string
-  end
-end
--- db/migrate/20260104062338_add_code_to_columns.rb
+++
@@ -1,6 +0,0 @@
-class AddCodeToColumns < ActiveRecord::Migration[6.1]
-  def change
-    add_column :columns, :code, :string
-    add_index :columns, :code, unique: true
-  end
-end
